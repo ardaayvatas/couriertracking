593ae2cea276d6e019e76036d4cfd04c46bfaded
ardaayvatas <ardaayvatas@gmail.com>
2025-05-20 04:24:08 +0300

    fixed issues

--- src/main/java/com/ardaayvatas/couriertracking/service/CourierLocationServiceImpl.java
+++ src/main/java/com/ardaayvatas/couriertracking/service/CourierLocationServiceImpl.java
@@ -11,7 +11,6 @@ import com.ardaayvatas.couriertracking.service.intf.CourierService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
